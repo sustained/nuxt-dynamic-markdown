43555fc569d21df1d8c1d129f7fe2dcd303b3353
sustained <sustained.dissonance@gmail.com>
2019-08-22 05:25:12 +0100

    Add rollup config.

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import vue from "rollup-plugin-vue";
+import cjs from "rollup-plugin-commonjs";
+
+export default {
+  input: "components/DynamicMarkdown.vue",
+  output: {
+    format: "esm",
+    file: "dist/DynamicMarkdown.js"
+  },
+  plugins: [vue(), cjs()]
+};
