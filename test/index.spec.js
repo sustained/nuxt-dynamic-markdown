63bdb34468c61341e1307792733f7b0c8f795dbc
sustained <sustained.dissonance@gmail.com>
2019-08-26 07:27:36 +0100

    test: Add basic tests for JSON generation.

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,14 +1,32 @@
+import { readdirSync, unlinkSync, existsSync } from "fs";
+import { resolve } from "path";
+
+const JSON_PATH = resolve(__dirname, "static/json");
+
 import chai, { expect, assert, should } from "chai";
 import chaiAsPromised from "chai-as-promised";
 chai.use(chaiAsPromised);
 
-import { processMarkdownSource } from "../lib/module.js";
+import { processMarkdownSource, writeJSONFiles } from "../lib/module.js";
 
 /*
   NOTE: We do this because sometimes we do are going to be expeted errors 
         to be thrown but the stack traces mess up the pretty test output.
 */
 process.on("unhandledRejection", () => {});
+
+/*
+  Empty out JSON directory.
+*/
+const files = readdirSync(JSON_PATH);
+
+for (const file of files) {
+  if (file === ".gitignore") {
+    continue;
+  }
+
+  unlinkSync(resolve(JSON_PATH, file));
+}
 
 describe("processMarkdownSource()", () => {
   describe("options parsing", () => {
@@ -48,11 +66,11 @@ describe("processMarkdownSource()", () => {
         relationships: ["languages", "technologies"]
       });
 
-      const { entities, relationships } = processed.projects;
+      const { entities: projects, relationships } = processed.projects;
       const { languages, technologies } = relationships;
 
       return (
-        expect(entities.length).to.equal(3) &&
+        expect(projects.length).to.equal(4) &&
         expect(Object.keys(languages).length).to.equal(3) &&
         expect(Object.keys(technologies).length).to.equal(4)
       );
@@ -73,6 +91,44 @@ describe("processMarkdownSource()", () => {
         expect(articles.length).to.equal(7) &&
         expect(categories.length).to.equal(2) &&
         expect(Object.keys(relationships.tags).length).to.equal(2)
+      );
+    });
+  });
+
+  describe("writing json", () => {
+    it("should write the json for the project source as expected", async () => {
+      const source = {
+        entities: ["projects"],
+        relationships: ["languages", "technologies"]
+      };
+      const processed = await processMarkdownSource(source);
+
+      writeJSONFiles(processed, source);
+
+      return (
+        expect(existsSync(resolve(JSON_PATH, "projects.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "languages.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "technologies.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "projects_languages.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "projects_technologies.json"))).to.equal(true)
+      );
+    });
+
+    it("should write the json for the example blog source as expected", async () => {
+      const source = {
+        entities: ["categories", "articles"],
+        directory: "blog",
+        relationships: ["tags"]
+      };
+      const processed = await processMarkdownSource(source);
+
+      writeJSONFiles(processed, source);
+
+      return (
+        expect(existsSync(resolve(JSON_PATH, "categories.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "articles.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "tags.json"))).to.equal(true) &&
+        expect(existsSync(resolve(JSON_PATH, "articles_tags.json"))).to.equal(true)
       );
     });
   });
